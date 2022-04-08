796f590bdfce8472f3403933c9d071d59a16d62b
Spoorthy M K <74946528+mkspoorthy2001@users.noreply.github.com>
2022-04-08 10:32:20 +0530

    Ruby learning

---
+++ intro_to_object_method.rb
@@ -0,0 +1,20 @@
+p "hello world".length
+a = 90
+a = p "hello #{a} world".upcase #assigns and prints the value
+p a.length
+b =  a.downcase
+p b
+p b.length
+#length method gives the length of the string including space and sspecial character.
+# but length method does not count #{} as it is a syntax
+#upcase for uppercase
+#downcase for lowercase
+puts
+# puts
+# puts
+p 20.next
+p -1.next
+
+p "hello world" #literal output with quotes
+puts "Hello world".inspect   # more literal output with quotes using inspect method with puts.
+puts "Hello world"
