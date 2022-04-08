35c75f2c3c1c432181229ee704e57c37b9f21933
Spoorthy M K <74946528+mkspoorthy2001@users.noreply.github.com>
2022-04-08 22:39:34 +0530

    Ruby learning

---
+++ gets_method.rb
@@ -0,0 +1,8 @@
+puts "Hi, what's your name?"
+name = gets.chomp
+puts "Great! what is your age?"
+age = gets.chomp.to_i
+p "Cool, ur name is #{name} and ur age is #{age}"
+# save the code and run on terminal
+# output after entering name and age is
+# "Cool, ur name is Spoorthy and ur age is 21"
