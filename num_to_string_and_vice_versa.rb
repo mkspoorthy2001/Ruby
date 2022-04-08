fd5e9631650c6b753804566ae54a2f1479d9ec68
Spoorthy M K <74946528+mkspoorthy2001@users.noreply.github.com>
2022-04-08 23:01:46 +0530

    Ruby learning

---
+++ num_to_string_and_vice_versa.rb
@@ -0,0 +1,18 @@
+str = "asdfg"
+p str.class
+p str.to_i.class
+#class helps to find the datatype of the variable.
+p str.to_f
+p str.to_f.class
+
+num = 10
+p num.class
+p num.to_s
+p num.to_s.class
+
+pi = 3.14159
+p pi.to_i
+p pi.to_i.class
+p pi.to_s
+
+p str.to_s
