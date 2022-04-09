3752482c3d26c949df2fd3f6a0aedd91d9625b7d
Spoorthy M K <74946528+mkspoorthy2001@users.noreply.github.com>
2022-04-09 22:57:09 +0530

    Ruby learning

---
+++ arithmetic_methods.rb
@@ -0,0 +1,11 @@
+p 2.next
+p 2.next()
+#paranthesis() are used when the method takes arguments or input
+p 1.+(9)  #p 1+2
+p 10.-(5)
+p 10.%(5)
+p 123./(5.0)
+p 10.div(5) #divide 10 by 5
+p 10.modulo(3)
+#only div and modulo method are these in ruby.
+# add, subtract or anything do not exist.
