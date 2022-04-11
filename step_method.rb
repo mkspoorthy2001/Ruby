5bf2946b202eb1d057ae45b7a50debd71f6b3be0
Spoorthy M K <74946528+mkspoorthy2001@users.noreply.github.com>
2022-04-11 10:18:29 +0530

    Ruby learning

---
+++ step_method.rb
@@ -0,0 +1,12 @@
+1.step(101,20) {|iteration| puts "#{iteration}"}
+# puts 2.step(10, 2)
+0.step(100, 15) do |n|
+puts "step #{n}"
+puts "Hurrey!"
+end
+
+
+0.step(10,1) {|n| print "#{n},"}
+puts
+0.upto(10) {|n| puts "#{n}"}
+0.upto(10) {|n| puts n}
