e785bb2ac8c073d42c041ca5088cb105f895ddd3
Spoorthy M K <74946528+mkspoorthy2001@users.noreply.github.com>
2022-05-24 09:52:11 +0530

    Ruby learning

---
+++ case_methods.rb
@@ -0,0 +1,14 @@
+puts "hello".capitalize
+puts "hELLo".capitalize
+puts "Hello".capitalize
+#returns first character uppercase and remaining characters lowercase
+puts "heLLo!@5{}".capitalize
+puts "hello world".capitalize
+puts "hello World".capitalize
+puts "hello World".capitalize.class
+
+puts "hello".upcase
+puts "ASDFGHJJ2345".downcase
+puts "SPOOrthy".swapcase
+puts "spoorthyKUMAR".swapcase.upcase.slice(-5,5)
+puts "spoorthy KUMAR".swapcase.upcase
---
+++ overwrite_characters_in_string.rb
@@ -0,0 +1,21 @@
+thing = "rocket ship"
+p thing
+thing[0] = "p"
+p thing
+thing[1] = "a"
+p thing
+thing[9] = "o"
+p thing
+
+fact  =  "I love blueberry pi"
+p fact
+fact[7,4] = "Rasp"
+p fact
+fact[7..10] = "Blue"
+p  fact
+fact[7...10] = "Raspp" # repalces 3 characters with 5 characters, e remains
+p fact
+fact[2,4] = "absolutely adore" #replcaing from 2nd index to next 4
+p fact
+fact[2,16] = "love"
+p fact
