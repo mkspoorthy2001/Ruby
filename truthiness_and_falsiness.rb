af62e2203e537a47ce92c41aad5db51518840523
Spoorthy M K <74946528+mkspoorthy2001@users.noreply.github.com>
2022-05-26 22:51:41 +0530

    Ruby learning

---
+++ truthiness_and_falsiness.rb
@@ -0,0 +1,27 @@
+if 5>3
+puts  "this is true"
+end
+if true
+puts "I am always true"
+end
+if false
+puts "i never execute, because I am false always"
+end
+
+if 5
+puts " 5 istrue"
+end
+if nil
+puts "nothing is executed"
+end
+# besides false and nil, everything in ruby is true
+#even 0 and -ve numbers are true values in ruby
+if 0
+puts "I(0) am true in ruby"
+end
+if "fghjkl"  #throws warning
+puts "kjhgfd"
+end
+# if 5..39
+# puts "true"
+# end
