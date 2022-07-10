490bbda24422ec55cdb1b0e08acbb70cd1ddb34f
Spoorthy M K <74946528+mkspoorthy2001@users.noreply.github.com>
2022-07-10 23:52:32 +0530

    Ruby learning

---
+++ index_and_find_index_method.rb
@@ -0,0 +1,7 @@
+colors = ["Red", "Blue", "Green", "Red"]
+p colors.index("Green")
+p colors.index("Red")
+p colors.find_index("Green")
+p colors.find_index("Orange")
+#find_index and index methods are same used to get the index of the particular element
+#both takes one argument
