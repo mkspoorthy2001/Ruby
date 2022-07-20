8300420e0747bc89656db843a3524948c1913f2b
Spoorthy M K <74946528+mkspoorthy2001@users.noreply.github.com>
2022-07-20 12:09:29 +0530

    Ruby learning

---
+++ the_length_and_empty_qm_methods.rb
@@ -0,0 +1,12 @@
+shopping_list = {a: 1, b: 2, c: 3, d:4}
+p shopping_list.length
+p shopping_list.empty?   #it is not empty, so returns false
+a = {}
+p a.empty?
+
+p arr = [1,2,3,4,5]
+p arr.empty?
+p [].empty?
+# p {}.empty?   #trows error
+# we = {1,2,3,4}
+# p we.empty?
