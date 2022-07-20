8b60f0d63f49dd0a0ec672fab61172005e73da7f
Spoorthy M K <74946528+mkspoorthy2001@users.noreply.github.com>
2022-07-20 11:48:58 +0530

    Ruby learning

---
+++ convert_symbols_to_str_and_vice_versa.rb
@@ -0,0 +1,9 @@
+p :age.to_s
+p "age".to_sym
+p :age.to_s.class
+p "age".to_sym.class
+p "School_bus".to_sym
+#no quotes if there are no spaces
+p "School bus".to_sym
+# "quotes appear after the colon if there are spaces "
+p :School_bus.to_s
